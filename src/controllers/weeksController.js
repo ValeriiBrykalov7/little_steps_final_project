a00ec68eeb67623149599a362e73d2719dd5d231
Anna Anishchenko <annuwkaanishchenko@gmail.com>
2026-04-23 20:42:36 +0300

    19_api_task

--- src/controllers/weeksController.js
+++ src/controllers/weeksController.js
@@ -1,1 +1,19 @@
 // WeeksController
+import createHttpError from 'http-errors';
+import { MomState } from '../models/momState.js';
+
+//mumStateByWeek
+export const getMumStateByWeek = async (req, res) => {
+  const { weekNumber } = req.params;
+
+  const data = await MomState.findOne({ weekNumber: Number(weekNumber) });
+  if (!data) {
+    createHttpError(404, 'Тиждень не знайдено');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Дані отримано успішно',
+    data,
+  });
+};
--- src/validations/weeksValidation.js
+++ src/validations/weeksValidation.js
@@ -1,1 +1,8 @@
 // WeeksValidation
+import { Joi, Segments } from 'celebrate';
+
+export const weekParamSchema = {
+  [Segments.PARAMS]: Joi.object({
+    weekNumber: Joi.number().min(1).required(),
+  }),
+};
