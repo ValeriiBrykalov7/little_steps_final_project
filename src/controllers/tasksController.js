7c63aec04d3e6c39ee77f6e20241f3828a03b609
andriy <and123bar@gmail.com>
2026-04-23 19:25:10 +0300

    add change status

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,1 +1,24 @@
 // TasksController
+import { Task } from '../models/task.js';
+
+export const updateTaskStatus = async (req, res, next) => {
+  const { taskId } = req.params;
+  const { isDone } = req.body;
+
+  try {
+
+    const result = await Task.findOneAndUpdate(
+      { _id: taskId, userId: req.user.id },
+      { isDone },
+      { new: true } 
+    );
+
+    if (!result) {
+      return res.status(404).json({ message: 'Task not found or unauthorized' });
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import { authenticate } from '../middleware/authenticate';
+import { updateTaskStatus } from '../controllers/tasksController.js';
+
+
 
 const router = Router();
+
+router.patch('/:taskId/status', authenticate, updateTaskStatus);
 
 export default router;
