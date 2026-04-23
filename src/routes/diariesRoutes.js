5506efdcb375f2f36ea4f97f2ae5ba5cb34beaf5
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2026-04-23 22:19:49 +0300

    add 12 and 13 task

--- src/routes/diariesRoutes.js
+++ src/routes/diariesRoutes.js
@@ -1,5 +1,19 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import { createDiary, getAllDiary } from '../controllers/diariesController.js';
+import { createDiarySchema } from '../validations/diariesValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
+
+// router.use('/api/diaries', authenticate);
+
+router.get('/allDiary', authenticate, getAllDiary);
+router.post(
+  '/createDiary',
+  authenticate,
+  celebrate(createDiarySchema),
+  createDiary,
+);
 
 export default router;
--- src/validations/diariesValidation.js
+++ src/validations/diariesValidation.js
@@ -1,1 +1,18 @@
 // DiariesValidation
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const createDiarySchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    description: Joi.string().min(1).max(1000).required(),
+    emotions: Joi.array()
+      .items(Joi.string().custom(objectIdValidator).required())
+      .min(1)
+      .required(),
+  }),
+};
