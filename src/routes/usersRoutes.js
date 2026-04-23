c2bfe7294696af95db14bf1a918fbd1a7c09bc4b
Hanna <hannamuzychuk@gmail.com>
2026-04-23 14:05:00 +0200

    add auth middleware and users/current private endpoint

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,16 @@
+import { celebrate } from 'celebrate';
 import { Router } from 'express';
+import { getCurrentUserController } from '../controllers/usersController.js';
+import { authenticate } from '../middleware/authenticate.js';
+import { getCurrentUserSchema } from '../validations/usersValidation.js';
 
 const router = Router();
+
+router.get(
+  '/current',
+  celebrate(getCurrentUserSchema),
+  authenticate,
+  getCurrentUserController,
+);
 
 export default router;
--- src/validations/usersValidation.js
+++ src/validations/usersValidation.js
@@ -1,1 +1,7 @@
-// UsersValidation
+import { Joi, Segments } from 'celebrate';
+
+export const getCurrentUserSchema = {
+  [Segments.BODY]: Joi.object({}).messages({
+    'object.base': 'Request body must be an object',
+  }),
+};
