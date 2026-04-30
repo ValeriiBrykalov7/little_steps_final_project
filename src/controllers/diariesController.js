8d05ce832830d243041c58ccfa88cd71494aede1
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2026-04-30 19:58:48 +0300

    add AllEmotion

--- src/controllers/diariesController.js
+++ src/controllers/diariesController.js
@@ -1,13 +1,15 @@
 // DiariesController
 import { Diary } from '../models/diary.js';
 import '../models/emotion.js';
+import { Emotion } from '../models/emotion.js';
 
 export const getAllDiary = async (req, res) => {
   const diary = await Diary.find({ userId: req.user._id }).populate(
     'emotions',
     'title',
   );
-  res.status(200).json(diary);
+  const allEmotions = await Emotion.find();
+  res.status(200).json({ diary, allEmotions });
 };
 
 export const createDiary = async (req, res) => {
